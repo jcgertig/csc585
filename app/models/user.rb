b17264689a42b59acf7fbc4bd8c61e9aacb526ac
Jonathan Gertig <jcgertig@gmail.com>
2013-10-16 15:11:32 -0500

    fix auth output

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
 def self.find_for_google_oauth(auth, signed_in_resource=nil)
   user = User.where(:provider => auth.provider, :uid => auth.uid).first
-  puts "auth" + auth
+  puts "auth" + auth.to_yaml
   unless user
     user = User.create(name:auth.extra.raw_info.name,
       provider:auth.provider,
