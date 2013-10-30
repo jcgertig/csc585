650f5b20ab87777c2eb7daeb71bbf97d1893713c
Jonathan Gertig <jcgertig@gmail.com>
2013-10-30 11:58:34 -0500

    states

---
+++ app/models/profile.rb
@@ -0,0 +1,13 @@
+class Profile < ActiveRecord::Base
+  attr_accessible :addr_line1, :addr_line2, :city,
+                  :contact, :first_name, :last_name,
+                  :phone, :state, :user_name, :zip_code, :user_id
+
+  def empty_fileds?
+    if addr_line1 == "" || first_name == "" || last_name == "" || user_name == "" || phone == "" || zip_code == ""
+      true
+    else
+      false
+    end
+  end
+end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,5 @@
 class State < ActiveRecord::Base
   attr_accessible :name, :sbbr
+
+  scope :by_name, order("name ASC")
 end
