185766958fd7b2e3b1dc9bb830a48ddf33a8d7b3
Jonathan Gertig <jcgertig@gmail.com>
2013-10-30 18:42:36 -0500

    fix formating

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
       if current_user.no_profile?
         redirect_to new_profile_path, notice: "Please fill in your profile information."
       elsif current_user.empty_fields?
-        redirect_to edit_profile_path(current_user.profile), notice: "Please fill in your profile information."
+        redirect_to edit_profile_path(current_user.profile), notice: "Please fill in all required fields."
       end	
     end
   end
