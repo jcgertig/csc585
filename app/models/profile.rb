0fcc715ed27c4a1375a035bbff8571d3119a86ad
Jonathan Gertig <jcgertig@gmail.com>
2013-10-30 16:29:47 -0500

    fix spelling

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
                   :contact, :first_name, :last_name,
                   :phone, :state, :user_name, :zip_code, :user_id
 
-  def empty_fileds?
+  def empty_fields?
     if addr_line1 == "" || first_name == "" || last_name == "" || user_name == "" || phone == "" || zip_code == ""
       true
     else
